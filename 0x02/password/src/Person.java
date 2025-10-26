448cd3144ecae2ab76117344272d70236f6c0fa0
EltonCampos07 <eltonapcnunes@gmail.com>
2025-10-26 18:48:55 -0300

    feat: finalização do exercício 3 de teste

---
+++ 0x02/password/src/Person.java
@@ -0,0 +1,15 @@
+import java.util.Objects;
+
+public class Person {
+
+    public static Boolean checkUser(String userName) {
+        return Objects.nonNull(userName) &&
+                userName.length() >= 8 &&
+                userName.matches("^[A-Za-z0-9]+$");
+    }
+
+    public static Boolean check_password(String password) {
+        return Objects.nonNull(password) &&
+                password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$");
+    }
+}
